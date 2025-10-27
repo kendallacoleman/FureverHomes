64e238e5416ab5cc2ba33c6d9e845d337e0e10cd
Kendall Coleman <kendallac04@gmail.com>
2025-10-27 14:10:08 -0400

    Deleted old files- only using frontend and backend now

--- fureverhomes/animal_detail/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('<int:animal_id>/', views.detail_view, name='animal_detail'),
-]
--- fureverhomes/search_animals/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Pet  # make sure you have a Pet model
-
-class PetSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Pet
-        fields = ['id', 'name', 'species', 'age', 'location', 'profile_picture']
