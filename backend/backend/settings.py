8a0093eee7cd13270a1e654d730b3979b08dc06c
Kendall Coleman <kendallac04@gmail.com>
2025-11-04 20:17:43 -0500

    changes to fix 400 error

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,7 +31,6 @@ SECRET_KEY = 'django-insecure-)c-l#&$$w32#ckxs6nmr*@o^_ip+gme4&m9bmuytt-ks1x956p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = ENVIRONMENT != 'production'
 
-# ALLOWED_HOSTS = ['https://fureverhomes-u8auv.ondigitalocean.app/', 'localhost', '127.0.0.1']
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
@@ -135,7 +134,7 @@ else:
         }
     }
 
-ALLOWED_HOSTS = ['https://fureverhomes-u8auv.ondigitalocean.app/', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['fureverhomes-g5esf.ondigitalocean.app', 'localhost', '127.0.0.1']
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
