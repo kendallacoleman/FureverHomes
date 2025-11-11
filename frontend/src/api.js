2dedfdc167beb9350ea6abbaa1e37dde453e50ff
Kendall Coleman <kendallac04@gmail.com>
2025-11-10 19:35:58 -0500

    changes to api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "./constants";
 
 const BASE_URL = import.meta.env.VITE_API_URL|| 'http://localhost:8000';
+console.log("Using API base URL:", BASE_URL);
 
 const api = axios.create({
   // baseURL: "http://localhost:8000", // Django backend
